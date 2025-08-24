990312e65b2458382ed2c7b18e6b6ebb3c0ba357
Subhiksha17 <subhikshavp@gmail.com>
2025-08-24 14:42:02 +0930

    Add files via upload
    day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ String name="John Doe";
+ short age=30;
+ float salary=55000.75f;
+ char gender='M';
+ boolean acceptTerms=true;
+ float rating=4.5f;
+ long mobileNumber= 9876543210L;
+ int pinCode=56001;
+ byte luckyNumber=7;
+ System.out.println("Employee Details \n Name:"+ name + 
+		 "\n Age:"+ age+
+		 "\n Salary:"+salary+
+		 "\n Gender:"+gender+
+		 "\n Accepted to terms and conditions:"+acceptTerms+
+		 "\n Rating:"+rating+
+		 "\n Mobile Number:"+mobileNumber+
+		 "\n Pincode:"+pinCode+
+		 "\n Lucky Number:"+luckyNumber);
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ float browserVersion=100.2f;
+ String browserName="firefox";
+ boolean isVisible=true;
+ short releaseYear= 1998;
+ char browserLogo='f';
+ System.out.println(browserVersion+"\n"+isVisible+"\n"+
+ browserName+"\n"+releaseYear+"\n"+browserLogo);
+	}
+
+}
