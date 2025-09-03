bd8f86d0ac364b32a9849233815d73e6e913834e
Subhiksha17 <subhikshavp@gmail.com>
2025-09-04 02:42:59 +0930

    Add files via upload
    week2day1

--- CreateAccount.java
+++ CreateAccount.java
@@ -1,46 +1,34 @@
-package week2.day1;
+package homeAssignment.week2;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
 
 public class CreateAccount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("guest");
-		
-		
-		ChromeDriver create = new ChromeDriver(options);
-		create.get("http://leaftaps.com/opentaps/");
-		create.manage().window().maximize();
-		create.findElement(By.id("username")).sendKeys("demosalesmanager");
-		create.findElement(By.id("password")).sendKeys("crmsfa");
-		create.findElement(By.className("decorativeSubmit")).click();
-		
-		create.findElement(By.partialLinkText("CRM")).click();
-		create.findElement(By.linkText("Leads")).click();
-		create.findElement(By.linkText("Create Lead")).click();
-		create.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
-		create.findElement(By.id("createLeadForm_firstName")).sendKeys("Subhiksha");
-		create.findElement(By.id("createLeadForm_lastName")).sendKeys("Vigneshwaran");
-		WebElement source= create.findElement(By.id("createLeadForm_dataSourceId"));
-		Select employee= new Select(source);
-		employee.selectByIndex(4);
-		WebElement marketing = create.findElement(By.id("createLeadForm_marketingCampaignId"));
-		Select automobile = new Select(marketing);
-		automobile.selectByVisibleText("Automobile");
-		WebElement ownership = create.findElement(By.id("createLeadForm_ownershipEnumId"));
-		Select corporation = new Select(ownership);
-		corporation.selectByValue("OWN_SCORP");
-		
-		create.findElement(By.className("smallSubmit")).click();
-		String title=create.getTitle();
-		System.out.println(title);
-		create.close();		
+		ChromeOptions pop= new ChromeOptions();
+		pop.addArguments("guest");
+		ChromeDriver tl= new ChromeDriver(pop);
+		tl.get("http://leaftaps.com/opentaps/");
+		tl.manage().window().maximize();
+		tl.findElement(By.id("username")).sendKeys("demosalesmanager");
+		tl.findElement(By.id("password")).sendKeys("crmsfa");
+		tl.findElement(By.className("decorativeSubmit")).click();
+		tl.findElement(By.partialLinkText("CRM")).click();
+		tl.findElement(By.linkText("Accounts")).click();
+		tl.findElement(By.linkText("Create Account")).click();
+		tl.findElement(By.id("accountName")).sendKeys("Subhiksha");
+		tl.findElement(By.name("description")).sendKeys("Selenium Automation Tester");
+		tl.findElement(By.id("numberEmployees")).sendKeys("6");
+	tl.findElement(By.id("officeSiteName")).sendKeys("LeafTaps");
+	tl.findElement(By.className("smallSubmit")).click();
+	System.out.println(tl.getTitle()); 
+	tl.close();
+	
+	
+	
+	
 	}
-
 }
